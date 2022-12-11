a0280606d9da068c5652e049d5bd1ce21108545e
choiseunghyeon <ket8780@gmail.com>
2022-12-11 21:43:07 +0900

    added showing alert when portfolio save

--- container/builder/SaveContainer.tsx
+++ container/builder/SaveContainer.tsx
@@ -1,16 +1,34 @@
 import { savePortfolioById } from "@lib/api/builder"
 import { usePortfolioeMutation } from "@lib/hooks/query"
-import React from "react"
+import { Alert } from "@mui/material"
+import React, { useEffect, useState } from "react"
 
 interface ISaveContainerProps {
   portfolioId: string
 }
+
 function SaveContainer({ portfolioId }: ISaveContainerProps) {
+  const [showAlert, setShowAlert] = useState(false)
   const portfolioeMutation = usePortfolioeMutation(portfolioId)
+
   const onClick = () => {
     portfolioeMutation.mutate()
+    setShowAlert(true)
   }
-  return <button onClick={onClick}>저장</button>
+
+  useEffect(() => {
+    if (showAlert) {
+      setTimeout(() => {
+        setShowAlert(false)
+      }, 2000)
+    }
+  }, [showAlert])
+  return (
+    <>
+      <button onClick={onClick}>저장</button>
+      {showAlert && <Alert severity="info">저장되었습니다.</Alert>}
+    </>
+  )
 }
 
 export default SaveContainer
